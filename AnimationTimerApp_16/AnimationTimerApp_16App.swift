1d428751f28edf8b9f0880d1b0f3b919e6bbe804
emm <723125@gmail.com>
2021-01-29 11:18:22 +0000

    Initial Commit

---
+++ AnimationTimerApp_16/AnimationTimerApp_16App.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationTimerApp_16App.swift
+//  AnimationTimerApp_16
+//
+//  Created by emm on 29/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct AnimationTimerApp_16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
